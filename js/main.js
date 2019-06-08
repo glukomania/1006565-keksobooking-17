b82ce93aa8fab805ddef7ac528b8d37cc9e58309
glukomania <glukomani@gmail.com>
2019-06-08 22:27:43 +0200

    Сделано все, кроме рандома аватарок

---
+++ js/main.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// Find pins in index.html
+var mapPins = document.querySelector('.map__pins');
+
+// Add type array
+var type = ['palace', 'flat', 'house', 'bungalo'];
+
+// random index of any array
+var randNumber = function (min, max) {
+  var random = Math.round(Math.random() * (max - min) + min);
+  return random;
+};
+
+// array of random nonrepeating numbers
+var randNonRepeating = [];
+var numberOfImages = 8;
+for (var i = 0; i < numberOfImages; i++) {
+  randNonRepeating.push(i);
+}
+
+// creating map pins array
+
+var getPin = function (n) {
+  n++;
+  var onePin = {
+    'author': {
+      'avatar': 'img/avatars/user0' + randNonRepeating[n]
+    },
+
+    'offer': {
+      'type': type[randNumber(0, 3)]
+    },
+
+    'location': {
+      'x': randNumber(0, mapPins.offsetWidth),
+      'y': randNumber(130, 630)
+    }
+  };
+  return onePin;
+};
+
+var pins = [];
+for (i = 0; i < 8; i++) {
+  pins.push(getPin(i));
+}
+
+// remove class .map--faded
+document.querySelector('.map').classList.remove('map--faded');
+
+// create DOM elements
+
+
+var pinTemplate = document.querySelector('#pin')
+    .content
+    .querySelector('button');
+
+for (var j = 0; j < pins.length; j++) {
+  var pinElement = pinTemplate.cloneNode(true);
+  pinElement.style = 'left: ' + pins[j].location.x + 'px; top: ' + pins[j].location.y + 'px;';
+  pinElement.src = pins[j].author.avatar;
+  pinElement.alt = 'заголовок объявления';
+  mapPins.appendChild(pinElement);
+
+}
