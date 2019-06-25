00af9a6bec18cf5c98cdecdfe3bd0b5716a6446c
glukomania <glukomani@gmail.com>
2019-06-25 23:38:03 +0200

    Правки

--- js/form.js
+++ js/form.js
@@ -90,9 +90,8 @@
 
   var errorHandler = function (errorMessage) {
     if (errorMessage) {
-      var errorDiv = document.querySelector('.error');
+      var errorDiv = document.querySelector('#error');
       var errorNode = errorDiv.cloneNode(true);
-      //var promo = document.querySelector('.promo');
       document.body.insertAdjacentElement('afterbegin', errorNode);
     }
   };
