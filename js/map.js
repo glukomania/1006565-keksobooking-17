db941e3ea1c91a61960a1c74808e5212e23d4423
glukomania <glukomani@gmail.com>
2019-06-30 12:28:08 +0200

    Работает

--- js/map.js
+++ js/map.js
@@ -1,18 +1,8 @@
 'use strict';
 
-// creating PINS elements from template
 // pins.js
-(function () {
-  // array of random nonrepeating numbers
-  var randNonRepeating = [];
-  var numberOfImages = 8;
-  for (var i = 1; i <= numberOfImages; i++) {
-    randNonRepeating.push(i);
-  }
-})();
 
 // set address
-
 (function () {
   window.setAddress = function (x, y) {
     var addressField = document.querySelector('#address');
@@ -23,32 +13,43 @@
 
 // Set initial and active mode
 (function () {
-  var adForm = document.querySelector('.ad-form');
-  var adFormSubmit = adForm.querySelector('.ad-form__submit');
-  var mapPins = document.querySelector('.map__pins');
 
   // Handlers of requests
+  var pins = [];
+  var housingType;
 
-  var successLoadHandler = function (pins) {
-    var pinTemplate = document.querySelector('#pin')
-    .content
-    .querySelector('button');
-    for (var j = 0; j < pins.length; j++) {
-      var pinElement = pinTemplate.cloneNode(true);
-      var x = pins[j].location.x - PIN_WIDTH / 2;
-      var y = pins[j].location.y - PIN_HEIGHT / 2;
-      pinElement.style = 'left: ' + x + 'px; top: ' + y + 'px;';
-      var pinImg = pinElement.querySelector('img');
-      pinImg.src = pins[j].author.avatar;
-      pinImg.alt = 'заголовок объявления';
-      mapPins.appendChild(pinElement);
+  // reaction on housing type clicking
+
+  var housingTypeSelector = document.querySelector('#housing-type');
+  housingTypeSelector.addEventListener('change', function () {
+    var mapPins = document.querySelector('.map__pins');
+    var pinElementsSeen = mapPins.querySelectorAll('.pin');
+    for (var i = 0; i < pinElementsSeen.length; i++) {
+      mapPins.removeChild(pinElementsSeen[i]);
     }
+    housingType = housingTypeSelector.value;
+    updatePins();
+  });
+
+  var updatePins = function () {
+    var sameHousingType = pins.filter(function (it) {
+      if (housingType === 'any') {
+        window.render(pins);
+      }
+      return it.offer.type === housingType;
+    });
+    window.render(sameHousingType);
+  };
+
+  var successLoadHandler = function (data) {
+    pins = data;
+    window.render(pins);
+    updatePins();
   };
 
   // SET INITIAL (disabled mode) STATE of the page
-
-  var PIN_WIDTH = 50;
-  var PIN_HEIGHT = 70;
+  var adForm = document.querySelector('.ad-form');
+  var adFormSubmit = adForm.querySelector('.ad-form__submit');
   var MAIN_ROUND_PIN_WIDTH = 35;
   var MAIN_ROUND_PIN_HEIGHT = 35;
   var adFormPhoto = adForm.querySelector('.ad-form__field');
@@ -83,13 +84,13 @@
       // Reset the form
       resetForm.disabled = false;
       var onResetLink = function () {
-        window.setActive(false, false);
+        window.setActive(false, true);
       };
       resetForm.addEventListener('click', onResetLink);
 
     } else {
-      if (isFirstOpen === false) {
-        var pinsElementInDOM = document.querySelectorAll('.map__pin');
+      if (isFirstOpen === true) {
+        var pinsElementInDOM = document.querySelectorAll('.pin');
         for (i = 0; i < pinsElementInDOM.length; i++) {
           pinsElementInDOM[i].style.visibility = 'hidden';
         }
@@ -125,70 +126,6 @@
   // set active mode!
   mouseup.addEventListener('mousedown', function () {
     window.setActive(true, false);
-  });
-})();
-
-// Drug'n'drop of the main pin
-(function () {
-  var MAIN_PIN_WIDTH = 65;
-  var MAIN_PIN_HEIGHT = 87;
-  var mouseup = document.querySelector('.map__pin--main');
-  var mapPins = document.querySelector('.map__pins');
-
-  mouseup.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
-
-    var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
-
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      // Set the limits for pin moving
-      if (mouseup.offsetTop > 630) {
-        mouseup.style.top = 630 + 'px';
-      } else if (mouseup.offsetTop < (130 - MAIN_PIN_HEIGHT / 2)) {
-        mouseup.style.top = (130 - MAIN_PIN_HEIGHT / 2) + 'px';
-      } else {
-        mouseup.style.top = (mouseup.offsetTop - shift.y) + 'px';
-      }
-      if (mouseup.offsetLeft >= (mapPins.offsetWidth - MAIN_PIN_WIDTH / 2)) {
-        mouseup.style.left = (mapPins.offsetWidth - MAIN_PIN_WIDTH / 2 - 1) + 'px'; // Here there's amagic number '- 1', which is really magic, because without it the pin doesn't wont to get out of the side =(
-      } else if (mouseup.offsetLeft <= 0 - (MAIN_PIN_WIDTH / 2)) {
-        mouseup.style.left = (0 - (MAIN_PIN_WIDTH / 2)) + 'px';
-      } else {
-        mouseup.style.left = (mouseup.offsetLeft - shift.x) + 'px';
-      }
-
-      // set the new address according to pin coordinates
-      window.setAddress(Math.round(mouseup.offsetLeft + MAIN_PIN_WIDTH / 2), Math.round(mouseup.offsetTop + MAIN_PIN_HEIGHT));
-    };
-
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-
-      window.setAddress(Math.round(mouseup.offsetLeft + MAIN_PIN_WIDTH / 2), Math.round(mouseup.offsetTop + MAIN_PIN_HEIGHT));
-
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-
-    };
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-
   });
 })();
 
---
+++ js/render.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// render pins
+
+(function () {
+  var PIN_WIDTH = 50;
+  var PIN_HEIGHT = 70;
+  var mapPins = document.querySelector('.map__pins');
+  var pinTemplate = document.querySelector('#pin')
+  .content
+  .querySelector('button');
+
+  var renderPin = function (pin) {
+    var pinElement = pinTemplate.cloneNode(true);
+    pinElement.classList.add('pin');
+    var pinImg = pinElement.querySelector('img');
+    pinImg.src = pin.author.avatar;
+    pinImg.alt = pin.offer.type;
+    var x = pin.location.x - PIN_WIDTH / 2;
+    var y = pin.location.y - PIN_HEIGHT / 2;
+    pinElement.style = 'left: ' + x + 'px; top: ' + y + 'px;';
+    return pinElement;
+  };
+
+  window.render = function (data) {
+    var takeNumber = data.length > 5 ? 5 : data.length;
+    for (var i = 0; i < takeNumber; i++) {
+      mapPins.appendChild(renderPin(data[i]));
+    }
+  };
+
+})();
