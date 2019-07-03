c080c2c08a192a997890525f81567e3cb1128195
glukomania <glukomani@gmail.com>
2019-07-03 21:28:53 +0200

    Фото добавляются, отображаются

---
+++ js/images.js
@@ -0,0 +1,69 @@
+'use strict';
+
+(function () {
+
+  // chooser of avatar
+
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var avatarChooser = document.querySelector('.ad-form-header__input');
+  var avatarPreview = document.querySelector('.ad-form-header__preview > img');
+
+  avatarChooser.addEventListener('change', function () {
+    var file = avatarChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        avatarPreview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+  // chooser of property photos
+
+  var photosChooser = document.querySelector('.ad-form__input');
+  var photosPreview = document.querySelector('.ad-form__photo');
+  var photosContainer = document.querySelector('.ad-form__photo-container');
+
+  photosChooser.addEventListener('change', function () {
+    var file = photosChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        var imgTag = document.createElement('img');
+        imgTag.src = reader.result;
+        imgTag.width = '70';
+        imgTag.height = '70';
+        if (document.querySelector('.ad-form__photo > img')) {
+          var moreDiv = photosPreview.cloneNode(false);
+          photosContainer.appendChild(moreDiv);
+          moreDiv.appendChild(imgTag);
+        } else {
+          photosPreview.appendChild(imgTag);
+        }
+      });
+
+      reader.readAsDataURL(file);
+    }
+
+  });
+
+
+})();
--- js/removeElement.js
+++ js/removeElement.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// utilite function of removing DOM elements
+
 (function () {
   window.removeElement = {
 
